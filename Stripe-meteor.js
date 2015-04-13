9b3aa81491d0fad9b36cf1fe361e4d185507f2a8
shivanireddy <shivani@wyrme.io>
2015-04-13 01:07:24 -0700

    Made a few changes in the create charges method

--- Stripe-meteor.js
+++ Stripe-meteor.js
@@ -9,20 +9,19 @@ if (Meteor.isClient) {
 
   function onCreationofToken(token){
     stripeToken = token.id;
+    stripeEmail = token.email;
     Customers.insert({
-      cusEmail: token.email,
+      cusEmail: stripeEmail,
       cusCard: stripeToken
     });
-    Meteor.call('chargeCard', stripeToken, price);
+    Meteor.call('chargeCard', stripeToken, price, stripeEmail);
   }
 
   var checkout = StripeCheckout.configure({
     key: 'pk_test_Z1eYlRSEH3ppUHqXxidAliOj', //test stripe publishable key
     // The callback after checkout is complete
     token: onCreationofToken
-
   });
-
 
 
   Template.list.helpers({
@@ -60,18 +59,33 @@ if (Meteor.isServer) {
   });
 
   Meteor.methods({
-    'chargeCard': function(stripeToken, price) {
+    'chargeCard': function(stripeToken, price, stripeEmail) {
       var Stripe = StripeAPI('sk_test_weYszsKrOxQB952IBECHyrLE'); //test stripe secret key
 
       Stripe.charges.create({
         amount: price,
         currency: 'usd',
-        card: stripeToken
+        card: stripeToken,
+        description: "Charge for "+ stripeEmail, //An arbitrary string which you can attach to a charge object. It is displayed when in the web interface alongside the charge. Note that if you use Stripe to send automatic email receipts to your customers, your receipt emails will include the description of the charge(s) that they are describing.
+        receipt_email: stripeEmail //The email address to send this charge's receipt to.
       }, function(err, res) {
         if(err){console.log(err);}
         else{console.log(res);
         return true;}
       });
     }
+    
+   // 'createCustomer': function(stripeToken, stripeEmail) {
+   //   var Stripe = StripeAPI('sk_test_weYszsKrOxQB952IBECHyrLE'); //test stripe secret key
+
+    //  Stripe.customers.create({
+    //    source: stripeToken,
+     //   description: "Charge for "+ stripeEmail, //An arbitrary string which you can attach to a charge object. It is displayed when in the web interface alongside the charge. Note that if you use Stripe to send automatic email receipts to your customers, your receipt emails will include the description of the charge(s) that they are describing.
+     // }, function(err, res) {
+    //    if(err){console.log(err);}
+    //    else{console.log(res);
+     //     return true;}
+     // });
+   // }
   });
 }
